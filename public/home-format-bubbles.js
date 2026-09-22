82464453cfbfe1b8221ea1c3aeb9950207533826
Wang106 <53130291+Wang106@users.noreply.github.com>
2026-09-22 21:57:01 +0800

    refactor: increase bubble cruise speed

--- public/home-format-bubbles.js
+++ public/home-format-bubbles.js
@@ -6,8 +6,8 @@
 
   const reduced=matchMedia('(prefers-reduced-motion: reduce)');
   const starts=[[.03,.08],[.27,.58],[.22,.1],[.61,.57],[.48,.04],[.06,.6],[.75,.13],[.88,.6]];
-  const velocities=[[.1,.069],[-.088,-.067],[.075,-.096],[-.104,.073],[.083,.061],[.093,-.08],[-.08,.088],[-.096,-.069]];
-  const idleSpeed=.06;
+  const velocities=[[.135,.093],[-.119,-.09],[.101,-.13],[-.14,.099],[.112,.082],[.126,-.108],[-.108,.119],[-.13,-.093]];
+  const idleSpeed=.085;
   let bodies=[];
   let frame=0;
   let previous=0;
